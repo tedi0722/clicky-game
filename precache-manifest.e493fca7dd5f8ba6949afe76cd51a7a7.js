fc015d5d10f16265bf43b8b3f133269ed90f30d0
tedi <tedi0722@users.noreply.github.com>
2019-01-28 20:56:31 -0800

    Updates

---
+++ precache-manifest.e493fca7dd5f8ba6949afe76cd51a7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "f575aa84b961a7bd06e0",
+    "url": "/clicky-game/static/js/main.f575aa84.chunk.js"
+  },
+  {
+    "revision": "2f572b4e296368269570",
+    "url": "/clicky-game/static/js/1.2f572b4e.chunk.js"
+  },
+  {
+    "revision": "f575aa84b961a7bd06e0",
+    "url": "/clicky-game/static/css/main.6050bda8.chunk.css"
+  },
+  {
+    "revision": "2d8fc2d4c43c789762380a61d5b1d394",
+    "url": "/clicky-game/index.html"
+  }
+];
